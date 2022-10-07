5508b230bb055231cdfc96472c53733b40bdf038
HelenaGingembre <e.izotovagingembre@outlook.com>
2022-10-07 20:26:55 +0300

    add style table

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import PropTypes from "prop-types";
 
+import css from './TransactionHistory.module.css';
+
 export const TransactionHistory = ({ items }) => (
-    <table class="transaction-history">
+    <table className={css.transaction-history}>
         <thead>
             <tr>
             <th>Type</th>
-            <th>Amount</th>
+            <th>Amount</th> 
             <th>Currency</th>
             </tr>
         </thead>
